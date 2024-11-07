c3a8802630dbd470b8acd5aa004c37d79a1dd7ba
Ostrovniy <ostrovniy0001@gmail.com>
2024-11-07 16:11:55 +0200

    MVC dome

--- tkinter/mvc.py
+++ tkinter/mvc.py
@@ -1,5 +1,12 @@
 # https://www.pythontutorial.net/tkinter/tkinter-mvc/
 import re
+import tkinter as tk
+from tkinter import ttk
+
+# (1) Если я нажымаю кнопку, и эта кнопка меняет цвет фона, измененяи цвета фона должно быть вызвано через Представления или КОнтроллер
+# (1) В представлении должна быть функция изменения цвета, а вызывает ее контроллер
+# (2) Методы которые определы в Модели, они должны или выполнять какое то действие внутри себя или выполнять его и верзвращать результат который будет принимать контроллре, при этом запуск метода происходит в контроллере ?
+# (2) Да
 
 class Model:
     def __init__(self, email):
@@ -37,7 +44,105 @@ class Model:
             f.write(self.email + '\n')
 
 
-test = Model('test@list.ru')
-print(test.email)
-test.email = 'seb@list.ru'
-print(test.email)
+#test = Model('test@list.ru') # (1) Сохздания
+#print(test.email) # (2) геттер
+#test.email = 'seb@list.ru' # (3) сеттер
+#print(test.email) # (4) геттер
+
+class View(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        # create widgets
+        # label - Заголовок для поля ввода
+        self.label = ttk.Label(self, text='Email:')
+        self.label.grid(row=1, column=0)
+
+        # email entry - Поле ввода почты
+        self.email_var = tk.StringVar()
+        self.email_entry = ttk.Entry(self, textvariable=self.email_var, width=30)
+        self.email_entry.grid(row=1, column=1, sticky=tk.NSEW)
+
+        # save button - Кнопка сохранить
+        self.save_button = ttk.Button(self, text='Save', command=self.save_button_clicked)
+        self.save_button.grid(row=1, column=3, padx=10)
+
+        # message - Текст валидации
+        self.message_label = ttk.Label(self, text='', foreground='red')
+        self.message_label.grid(row=2, column=1, sticky=tk.W)
+
+        # Поумолчанию контроллер пустой
+        self.controller = None
+
+    def set_controller(self, controller):
+        """Установка контроллера для Представления"""
+        self.controller = controller
+
+    def save_button_clicked(self):
+        """Обработка нажатия на кнопку"""
+        if self.controller:
+            self.controller.save(self.email_var.get())
+
+    def show_error(self, message):
+        """Ситуация когда ввели не валидный текст"""
+        self.message_label['text'] = message
+        self.message_label['foreground'] = 'red'
+        self.message_label.after(3000, self.hide_message)
+        #self.email_entry['foreground'] = 'red'
+
+    def show_success(self, message):
+        """Ситуация когда ввели валидный текст"""
+        self.message_label['text'] = message
+        self.message_label['foreground'] = 'green'
+        self.message_label.after(3000, self.hide_message)
+
+        # reset the form
+        self.email_entry['foreground'] = 'black'
+        self.email_var.set('')
+
+    def hide_message(self):
+        """Очистить поле ввода"""
+        self.message_label['text'] = ''
+
+class Controller:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+    def save(self, email):
+        try:
+            # Плпытка утановить email с валидацией + сохранения в БД
+            self.model.email = email
+            self.model.save()
+
+            # Обращаемся к представлению и меняем Стиль текста на успешный
+            self.view.show_success(f'The email {email} saved!')
+
+        except ValueError as error:
+            # Обращаемся к представлению и меняем Стиль текста на ошибку
+            self.view.show_error(error)
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Tkinter MVC Demo')
+
+        # (1) Создание модели
+        model = Model('hello@pythontutorial.net')
+
+        # (2) Создания представления
+        view = View(self)
+        view.grid(row=0, column=0, padx=10, pady=10)
+
+        # (3) Создания контроллера
+        controller = Controller(model, view)
+
+        # (4) Установка контроллера
+        view.set_controller(controller)
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
